2baa14f401234354be216417c2f5570583437762
JToeps <jim.toepel@gmail.com>
2017-10-11 20:27:07 -0700

    Popover complete

--- Quotes/AppDelegate.swift
+++ Quotes/AppDelegate.swift
@@ -12,15 +12,16 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
-
+    let popover = NSPopover()
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         if let button = statusItem.button {
             button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
-            button.action = #selector(printQuote(_:))
+            button.action = #selector(togglePopover(_:))
         }
         
-        constructMenu()
+        popover.contentViewController = QuotesViewController.freshController()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -42,6 +43,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         menu.addItem(NSMenuItem(title: "Quit Quotes", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
         
         statusItem.menu = menu
+    }
+    
+    @objc func togglePopover(_ sender: Any?) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
+    }
+    
+    func showPopover(sender: Any?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+    }
+    
+    func closePopover(sender: Any?) {
+        popover.performClose(sender)
     }
 
 }
--- Quotes/QuotesViewController.swift
+++ Quotes/QuotesViewController.swift
@@ -15,4 +15,20 @@ class QuotesViewController: NSViewController {
         // Do view setup here.
     }
     
+    
+}
+
+extension QuotesViewController {
+    // Storyboard instantiation
+    static func freshController() -> QuotesViewController {
+        // 1
+        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
+        // 2
+        let identifier = NSStoryboard.SceneIdentifier(rawValue: "QuotesViewController")
+        // 3
+        guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? QuotesViewController else {
+            fatalError("Why cant I find QuotesViewContoller? - Check Main.storyboard")
+        }
+        return viewcontroller
+    }
 }
