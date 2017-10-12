556c8377e899558d73d238df0c081c8bf643919c
JToeps <jim.toepel@gmail.com>
2017-10-11 19:49:23 -0700

    Added ViewController

---
+++ Quotes/QuotesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QuotesViewController.swift
+//  Quotes
+//
+//  Created by Jim Toepel on 10/11/17.
+//  Copyright © 2017 Jim Toepel. All rights reserved.
+//
+
+import Cocoa
+
+class QuotesViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
